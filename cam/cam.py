8eaa938f986ca435f21dcfa8907ab04539fe3ec3
Tom Bertalan <tom@tombertalan.com>
2015-10-20 21:20:41 -0400

    Handle multiple cameras.
    Coarsely.

--- cam/cam.py
+++ cam/cam.py
@@ -1,50 +1,60 @@
+#!/usr/bin/env python
 import pygame
 import pygame.camera
 from pygame.locals import *      
 from time import time, sleep
-from os import system
+from os import system, listdir
 from sys import argv
 
 pygame.init()                    
 pygame.camera.init()             
 
-print "cameras:", pygame.camera.list_cameras()
+cameras = pygame.camera.list_cameras()[:1]  # We only have enough USB bandwidth for 1 camera, it seems.
+print "cameras:", cameras
 
-cam = pygame.camera.Camera("/dev/video0",(640,480))                                  
-cam.start()
+cams = [pygame.camera.Camera(camPath, (640,480))
+        for camPath in cameras]
+for cam in cams:
+    cam.start()
 
 # Take a few pictures to get the brightness set correctly.
 for i in range(4):
-	cam.get_image()
+    for cam in cams:
+    	cam.get_image()
 	sleep(1)
 
-if argv[1]:
-    dated = True
+dated = len(argv) > 1 and argv[1]
 
-if argv[2]:
+if len(argv) > 2 and argv[2]:
     continuous = True
     sessionIdentifier = str(time())
     system("ssh sigurd.tomsb.net mkdir -p /home/tsbertalan/cam/%s" % sessionIdentifier)
+else:
+    continuous = False
 
-def sendImage(dated=False, flip=False, rotate=90):
-    image = cam.get_image()
-    now = time()
-    if flip:
-        image = pygame.transform.flip(image, False, True)  # flip vertically
-    if rotate:
-        image = pygame.transform.rotate(image, rotate)  # rotate CCW
+def sendImage(dated=False, flip=False, rotate=-90):
+    for i, cam in enumerate(cams):
+        image = cam.get_image()
+        now = time()
+        if flip:
+            image = pygame.transform.flip(image, False, True)  # flip vertically
+        if rotate:
+            if not i:  # cam1 should be rotated cw, not ccw
+                rotate *= -1
+            image = pygame.transform.rotate(image, rotate)  # rotate CCW
 
-    fpaths = ["/tmp/cam.jpg"]
-    if dated:
-        fpaths.append("/tmp/cam_%s.jpg" % now)
-    for fpath in fpaths:
-        pygame.image.save(image, fpath)
-        cmd = "scp %s sigurd.tomsb.net:~/cam/" % fpath
-        print "fpath is ", fpath
-        if continuous and "_" in fpath:
-            cmd += "%s/" % sessionIdentifier
-        print cmd
-        system(cmd)
+        fpaths = ["/tmp/cam%d.jpg" % i]
+        if dated:
+            fpaths.append("/tmp/cam%d_%s.jpg" % (i, now))
+        system("rm /tmp/cam*.jpg")
+        for fpath in fpaths:
+            pygame.image.save(image, fpath)
+            cmd = "scp %s sigurd.tomsb.net:~/cam/" % fpath
+            print "fpath is ", fpath
+            if continuous and "_" in fpath:
+                cmd += "%s/" % sessionIdentifier
+            print cmd
+            system(cmd)
 
 while True:
     sendImage(dated=dated)
@@ -52,5 +62,6 @@ while True:
         break
     sleep(.01)
 
-cam.stop()
+for cam in cams:
+    cam.stop()
 
