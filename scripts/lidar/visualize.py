75c33698f22a048e16314396251c63d264325f00
Tom Bertalan <tom@tombertalan.com>
2015-11-28 23:18:44 -0500

    Make the Visualizer a class.
    It can display data now, but it's not very fluid. Should use different
    processes for GUI and server. This is low priority, though.

--- scripts/lidar/visualize.py
+++ scripts/lidar/visualize.py
@@ -2,108 +2,106 @@
 # based on code from Nicolas "Xevel" Saugnier
 # requires vpython
 
-from threading import Thread
+from threading import Thread  # TODO: Use processes to make the GUI more fluid.
 import logging
 
 import numpy as np
+from visual import *
 
 from server import Server, QueueHandler
 
-timeout = np.inf
 
-offset = 140
-init_level = 0
-index = 0
+class Visualizer:
 
-lidarData = [[] for i in range(360)]  # A list of 360 elements Angle, Distance , quality
+    def __init__(self, handler):
+        self.lidarData = [[] for i in range(360)]  # A list of 360 elements Angle, Distance , quality
+        offset = self.offset = 140
+        init_level = 0
+        index = 0
 
-allData = []
+        # sample and intensity points
+        self.point = points(pos=[(0, 0, 0) for i in range(360)], size=5, color=(0 , 1, 0))
+        self.pointb = points(pos=[(0, 0, 0) for i in range(360)], size=5, color=(0.4, 0, 0))
+        self.point2 = points(pos=[(0, 0, 0) for i in range(360)], size=3, color=(1 , 1, 0))
+        self.point2b = points(pos=[(0, 0, 0) for i in range(360)], size=3, color=(0.4, 0.4, 0))
 
+        # lines
+        self.outer_line = curve (pos=[(0, 0, 0) for i in range(360)], size=5, color=(1 , 0, 0))
+        self.lines = [curve(pos=[(offset * cos(i * pi / 180.0), 0, offset * -sin(i * pi / 180.0)), (offset * cos(i * pi / 180.0), 0, offset * -sin(i * pi / 180.0))], color=[(0.1, 0.1, 0.2), (1, 0, 0)]) for i in range(360)]
+        self.zero_intensity_ring = ring(pos=(0, 0, 0), axis=(0, 1, 0), radius=offset - 1, thickness=1, color=color.yellow)
 
-from visual import *
-# sample and intensity points
-point = points(pos=[(0, 0, 0) for i in range(360)], size=5, color=(0 , 1, 0))
-pointb = points(pos=[(0, 0, 0) for i in range(360)], size=5, color=(0.4, 0, 0))
-point2 = points(pos=[(0, 0, 0) for i in range(360)], size=3, color=(1 , 1, 0))
-point2b = points(pos=[(0, 0, 0) for i in range(360)], size=3, color=(0.4, 0.4, 0))
-# lines
-outer_line = curve (pos=[(0, 0, 0) for i in range(360)], size=5, color=(1 , 0, 0))
-lines = [curve(pos=[(offset * cos(i * pi / 180.0), 0, offset * -sin(i * pi / 180.0)), (offset * cos(i * pi / 180.0), 0, offset * -sin(i * pi / 180.0))], color=[(0.1, 0.1, 0.2), (1, 0, 0)]) for i in range(360)]
-zero_intensity_ring = ring(pos=(0, 0, 0), axis=(0, 1, 0), radius=offset - 1, thickness=1, color=color.yellow)
+        self.label_speed = label(pos=(0, -500, 0), xoffset=1, box=False, opacity=0.1)
+        self.label_errors = label(pos=(0, -1000, 0), xoffset=1, text="errors: 0", visible=False, box=False)
 
-label_speed = label(pos=(0, -500, 0), xoffset=1, box=False, opacity=0.1)
-label_errors = label(pos=(0, -1000, 0), xoffset=1, text="errors: 0", visible=False, box=False)
-
-use_points = True
-use_outer_line = False
-use_lines = False
-use_intensity = True
+        self.use_points = True
+        self.use_outer_line = True
+        self.use_lines = True
+        self.use_intensity = True
 
 
-def update_view(angle, dist_mm, quality):
-    """Updates the view of a sample.
+    def update_view(self, angle, dist_mm, quality):
+        """Updates the view of a sample.
 
-Takes the angle (an int, from 0 to 359) and the list of four bytes of data in the order they arrived.
-"""
-#     logging.info("update_view(%s, %s, %s)" % (angle, dist_mm, quality))
-    global offset, use_outer_line, use_line
-    # unpack data using the denomination used during the discussions
+    Takes the angle (an int, from 0 to 359) and the list of four bytes of data in the order they arrived.
+    """
+    #     logging.info("update_view(%s, %s, %s)" % (angle, dist_mm, quality))
+        # unpack data using the denomination used during the discussions
 
-    angle_rad = angle * math.pi / 180.0
-    c = math.cos(angle_rad)
-    s = -math.sin(angle_rad)
+        angle_rad = angle * math.pi / 180.0
+        c = math.cos(angle_rad)
+        s = -math.sin(angle_rad)
 
-    lidarData[angle] = [dist_mm, quality]
-    dist_x = dist_mm * c
-    dist_y = dist_mm * s
+        self.lidarData[angle] = [dist_mm, quality]
+        dist_x = dist_mm * c
+        dist_y = dist_mm * s
 
-    # reset the point display
-    point.pos[angle] = vector(0, 0, 0)
-    pointb.pos[angle] = vector(0, 0, 0)
-    point2.pos[angle] = vector(0, 0, 0)
-    point2b.pos[angle] = vector(0, 0, 0)
-    if not use_lines : lines[angle].pos[1] = (offset * c, 0, offset * s)
-    if not use_outer_line :
-        outer_line.pos[angle] = (offset * c, 0, offset * s)
-        outer_line.color[angle] = (0.1, 0.1, 0.2)
+        # reset the point display
+        self.point.pos[angle] = vector(0, 0, 0)
+        self.pointb.pos[angle] = vector(0, 0, 0)
+        self.point2.pos[angle] = vector(0, 0, 0)
+        self.point2b.pos[angle] = vector(0, 0, 0)
+        if not self.use_lines : self.lines[angle].pos[1] = (self.offset * c, 0, self.offset * s)
+        if not self.use_outer_line :
+            self.outer_line.pos[angle] = (self.offset * c, 0, self.offset * s)
+            self.outer_line.color[angle] = (0.1, 0.1, 0.2)
 
-    # display the sample
-    if use_lines : lines[angle].pos[1] = (dist_x, 0, dist_y)
-    if use_outer_line : outer_line.pos[angle] = (dist_x, 0, dist_y)
-
-
-def read_Lidar(handler):
-    angles = range(360)
-    while True:
-        try:
-            scan = handler.dequque()
-            if isinstance(scan, np.ndarray) and scan.size > 0:
-                for angle, distQual in zip(angles, scan):
-                    if len(distQual) == 2:
-                        dist_mm, quality = distQual
-                        update_view(angle, dist_mm, quality)
-        except KeyboardInterrupt:
-            break
+        # display the sample
+        if self.use_lines : self.lines[angle].pos[1] = (dist_x, 0, dist_y)
+        if self.use_outer_line : self.outer_line.pos[angle] = (dist_x, 0, dist_y)
 
 
-def checkKeys():
-    global use_outer_line, use_lines, use_points, use_intensity
-    if scene.kb.keys:  # event waiting to be processed?
-        s = scene.kb.getkey()  # get keyboard info
+    def read_Lidar(self):
+        angles = range(360)
+        while True:
+            try:
+                scan = handler.dequque()
+                if isinstance(scan, np.ndarray) and scan.size > 0:
+                    for angle, distQual in zip(angles, scan):
+                        if len(distQual) == 2:
+                            dist_mm, quality = distQual
+                            self.update_view(angle, dist_mm, quality)
+                self.checkKeys()
+            except KeyboardInterrupt:
+                break
 
-        if s == "o":  # Toggle outer line
-            use_outer_line = not use_outer_line
-        elif s == "l":  # Toggle rays
-            use_lines = not use_lines
-        elif s == "p":  # Toggle points
-            use_points = not use_points
-        elif s == "i":  # Toggle intensity
-            use_intensity = not use_intensity
-            zero_intensity_ring.visible = use_intensity
-        elif s == "j":  # Toggle rpm
-            label_speed.visible = not label_speed.visible
-        elif s == "k":  # Toggle errors
-            label_errors.visible = not label_errors.visible
+
+    def checkKeys(self):
+        if scene.kb.keys:  # event waiting to be processed?
+            s = scene.kb.getkey()  # get keyboard info
+
+            if s == "o":  # Toggle outer line
+                use_outer_line = not self.use_outer_line
+            elif s == "l":  # Toggle rays
+                use_lines = not self.use_lines
+            elif s == "p":  # Toggle points
+                use_points = not self.use_points
+            elif s == "i":  # Toggle intensity
+                use_intensity = not self.use_intensity
+                self.zero_intensity_ring.visible = use_intensity
+            elif s == "j":  # Toggle rpm
+                self.label_speed.visible = not self.label_speed.visible
+            elif s == "k":  # Toggle errors
+                self.label_errors.visible = not self.label_errors.visible
 
 
 if __name__ == "__main__":
@@ -111,9 +109,10 @@ if __name__ == "__main__":
 
     handler = QueueHandler()
     server = Server(handler)
+    visualizer = Visualizer(handler)
 
     serverThread = Thread(target=server.serve)
-    visualizationThread = Thread(target=read_Lidar, args=(handler,))
+    visualizationThread = Thread(target=visualizer.read_Lidar)
     serverThread.start()
     visualizationThread.start()
 
