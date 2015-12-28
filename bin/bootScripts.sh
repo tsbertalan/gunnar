7c87488e3665ef62d224e77db2794a95f6f04f58
Tom Bertalan <tom@tombertalan.com>
2015-12-28 17:25:17 -0500

    Remove camera upload from boot script.

--- bin/bootScripts.sh
+++ bin/bootScripts.sh
@@ -5,8 +5,8 @@ scrCmd="/usr/bin/screen -dmS"
 cmd="$scrCmd tunnel-screen $HOME/bin/reverseTunnelSigurdStartup.sh"
 echo \$ $cmd
 $cmd 2>$HOME/Desktop/tunnelError.out
-sleep 1
-cmd="$scrCmd upload $HOME/sketchbook/gunnar/cam/uploadLastImageDir.py"
-echo \$ $cmd
-$cmd 2>$HOME/Desktop/uploadError.out
+#sleep 1
+#cmd="$scrCmd upload $HOME/sketchbook/gunnar/cam/uploadLastImageDir.py"
+#echo \$ $cmd
+#$cmd 2>$HOME/Desktop/uploadError.out
 
