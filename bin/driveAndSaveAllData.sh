f431de4270ecd580b99f4d73fc43642ec60e9101
Tom Bertalan <tom@tombertalan.com>
2015-12-28 19:09:47 -0500

    Add scripts for starting and stopping drive-and-save.
    However, it would make much more sense not to send LIDAR data over the
    network unnecessesarily. I have a new class design in mind for this.

---
+++ bin/driveAndSaveAllData.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+setPath="source $HOME/sketchbook/gunnar/bin/setPath.sh"
+$setPath
+HOST=sigurd.tomsb.net
+
+echo "Starting LIDAR server on $HOST."
+screen -dmS lidarServerSigurd ssh $HOST "$HOME/sketchbook/gunnar/bin/startLidarServer"
+screen -list
+sleep 8
+
+echo "Starting LIDAR client on "`hostname`"."
+screen -dmS lidarClientGunnar python $HOME/sketchbook/gunnar/src/python/gunnar/lidar/lidarClient.py $HOST
+screen -list
+sleep 8
+
+echo "Starting controller on "`hostname`"."
+screen -S controller python $HOME/sketchbook/gunnar/bin/driveAndSaveOdometry.py
+
