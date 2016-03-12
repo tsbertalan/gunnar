95fb6391edc10feb9a8dc9a0f0080f22736b6368
Tom Bertalan <tom@tombertalan.com>
2016-03-12 13:02:07 -0500

    Advance to a more elaborate test.

--- bin/testing.py
+++ bin/testing.py
@@ -1,6 +1,6 @@
 import unittest
 from gunnar.testing import TestGunnar
 ts = unittest.TestSuite()
-ts.addTest(TestGunnar('test_encodersNoMotors'))
+ts.addTest(TestGunnar('test_motorObjectMovement'))
 unittest.TextTestRunner().run(ts)
 
