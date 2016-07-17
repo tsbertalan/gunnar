1333fd535da81d1075cd4782a6876ea3e042b2e6
Tom Bertalan <tom@tombertalan.com>
2016-07-17 09:02:49 -0400

    Make keyboard_teleop pretend to use the cmd_vel topic.

--- scripts/keyboard_teleop.py
+++ scripts/keyboard_teleop.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
+import rospy
+from time import sleep
 
+from geometry_msgs.msg import Twist
+pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)
+
+while True:
+    for control_speed in range(10):
+        twist = Twist()
+        twist.linear.x = control_speed;
+        twist.linear.y = 0;
+        twist.linear.z = 0
+        twist.angular.x = 0;
+        twist.angular.y = 0;
+        twist.angular.z = control_turn
+        pub.publish(twist)
+    sleep(.1)
