77d39574a2bf5adf538616be0339809f32086ca5
Tom Bertalan <tom@tombertalan.com>
2016-07-19 10:59:15 -0400

    Make keyboard_teleop publish dummy data.

--- scripts/keyboard_teleop.py
+++ scripts/keyboard_teleop.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
-import rospy
 from time import sleep
-
+import rospy
 from geometry_msgs.msg import Twist
+
+rospy.init_node('keyboard_teleop')
+
 pub = rospy.Publisher('~cmd_vel', Twist, queue_size=5)
 
 while True:
     for control_speed in range(10):
         twist = Twist()
-        twist.linear.x = control_speed;
-        twist.linear.y = 0;
+        twist.linear.x = control_speed
+        twist.linear.y = 0
         twist.linear.z = 0
-        twist.angular.x = 0;
-        twist.angular.y = 0;
-        twist.angular.z = control_turn
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
         pub.publish(twist)
-    sleep(.1)
+        sleep(.1)
+
