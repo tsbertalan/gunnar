a42d1842667aede8ecf6397f05d696dae4869ece
Tom Bertalan <tom@tombertalan.com>
2016-07-28 21:51:42 -0400

    Convert make variables to bash variables.

--- scripts/utils/doFlash.sh
+++ scripts/utils/doFlash.sh
@@ -10,6 +10,6 @@ python scripts/utils/deviceInfo.py $SDX
 echo
 echo "MAKE SURE THE CORRECT DEVICE IS CHOSEN!!!"
 echo "Press return to continue or Ctrl+C to abort."; read
-sudo parted --script $(SDX) mkpart primary ext4 5G || true
-sudo dcfldd bs=4M if=$(IMGPATH) of=$(SDX)
+sudo parted --script $SDX mkpart primary ext4 5G || true
+sudo dcfldd bs=4M if=$IMGPATH of=$SDX
 sync
