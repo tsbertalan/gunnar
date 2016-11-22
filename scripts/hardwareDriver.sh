6d85077bf02973c9d36792ab5cb7835a0ce1818b
Tom Bertalan <tom@tombertalan.com>
2016-11-22 00:49:31 -0500

    Do basic odometry publishing.
    Data is wrong, though. Calibration and such.

--- scripts/hardwareDriver.sh
+++ scripts/hardwareDriver.sh
@@ -2,4 +2,4 @@
 >&2 echo "This file is $0."
 thisDir=`dirname $0`
 >&2 echo "This dir is $thisDir."
-sudo -E PYTHONPATH=$PYTHONPATH $thisDir/hardwareDriver
+sudo -E PYTHONPATH=$PYTHONPATH LD_LIBRARY_PATH=$LD_LIBRARY_PATH $thisDir/hardwareDriver
