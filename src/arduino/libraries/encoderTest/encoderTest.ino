b124e44fbcfd846c770a2a9db44088c2a8862a99
Tom Bertalan <tom@tombertalan.com>
2016-12-17 08:43:40 -0500

    Read right encoder too.
    Doesn't work on A4/A5. attachInterrupt only works on pins 2, 3, 18, 19,
    20, and 21 on the mega2560. The story might be different if we're using
    raw interrupt vectors, as in the ros_arduino_bridge code.

--- src/arduino/libraries/encoderTest/encoderTest.ino
+++ src/arduino/libraries/encoderTest/encoderTest.ino
@@ -4,16 +4,26 @@ Encoder encoderl;
 void encoderlIsr() {
     encoderl.update();
 }
+Encoder encoderr;
+void encoderrIsr() {
+    encoderr.update();
+}
 
 void setup() {
     Serial.begin(57600);
     encoderl.init(2, 3);
     attachInterrupt(digitalPinToInterrupt(encoderl.pin_a), encoderlIsr, CHANGE);
     attachInterrupt(digitalPinToInterrupt(encoderl.pin_b), encoderlIsr, CHANGE);
+    encoderr.init(20, 21);
+    attachInterrupt(digitalPinToInterrupt(encoderr.pin_a), encoderrIsr, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(encoderr.pin_b), encoderrIsr, CHANGE);
 }
 
 void loop() {
     delay(100);
-    Serial.println(encoderl.position);
+    Serial.print(encoderl.position);
+    Serial.print(" ");
+    Serial.print(encoderr.position);
+    Serial.println("");
 }
 
